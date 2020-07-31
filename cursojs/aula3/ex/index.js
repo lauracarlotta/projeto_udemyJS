60a682b1f14794bdb65ddf615f7808c98264b359
Laura Carlotta <carlotta.custodio@gmail.com>
2020-07-31 17:28:14 -0300

    introduçao ao console.log

---
+++ cursojs/aula3/ex/index.js
@@ -0,0 +1,11 @@
+/* Ex: No console deve aparecer assim:
+Meu nome é "NOME". Estou aprendendo JavaScript às 10 horas da manhã.
+
+Eu resolvi assim: */
+
+const nome ="Laura";
+
+console.log('Meu nome é "', nome, '". Estou aprendendo JavaScript às', 17, 'horas da tarde.');
+
+/* No caso, o Luiz Otávio apenas usou o console.log! */
+console.log('Meu nome é "Laura". Estou aprendendo JavaScript às', 17, 'horas da tarde!');
--- cursojs/aula3/index.js
+++ cursojs/aula3/index.js
@@ -1,1 +1,10 @@
-console.log(""); /* Para printar coisas na tela */
+console.log('Primeira aula prática'); // string - texto ou cadeia de caracteres => por ser de tipagem dinaica o motor do js já configurou o tipo para string
+console.log("Verificando como ficam as aspas simples e dulpas");
+console.log(`Verificando a 3ª possibilidade que é usando as crases!`);
+
+/* console.log('Primeira aula "prática"'); => Para printar coisas na tela 
+console.log("Verificando como ficam as 'aspas simples' e dulpas");
+console.log(`Verificando a "3ª possibilidade" que é 'usando' as crases!`); */
+
+console.log(123456, 23.99); /* int =  numeros inteiros <= Para a exibição de números apenas os coloca entre os parênteses! */
+console.log(23.99); /* float = (flutuante) numeros reias (mesmas coisa números inteiros) */ 
