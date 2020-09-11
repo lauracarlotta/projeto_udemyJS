cecf9e1d1f43c5a2ac8d9a96ec254f1fcaa267b9
Laura Carlotta <carlotta.custodio@gmail.com>
2020-09-11 15:03:19 -0300

    alert, comfirm and prompt

---
+++ cursojs/aula11/js/index.js
@@ -0,0 +1,6 @@
+let numero1 = prompt("Digite um número: ");
+let numero2 = prompt("Digite outro número: ");
+numero1 = Number(numero1);
+numero2 = Number(numero2);
+const soma = numero1 + numero2
+alert(`A soma de ${numero1} + ${numero2} é igual a ${soma}!`)
