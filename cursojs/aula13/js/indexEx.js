c8a488144e2b5ed0b8fd177f698c63f89917bb4e
Laura Carlotta <carlotta.custodio@gmail.com>
2020-09-15 13:16:22 -0300

    ex strings

---
+++ cursojs/aula13/js/indexEx.js
@@ -0,0 +1,10 @@
+const nome = prompt('Digite o seu nome completo: ')
+    document.body.innerHTML += `O seu nome é <strong><em>${nome}</em></strong>. <br/>`
+    document.body.innerHTML += `O seu nome tem <strong><em>${nome.length}</em></strong> letras. <br/>`
+    document.body.innerHTML += `A segunda letra do seu nome é <strong><em>${nome.toUpperCase()[1]}</em></strong>. <br/>`
+    document.body.innerHTML += `Qual o primeiro índice da letra A do seu nome? <strong><em>${nome.indexOf('a')}</em></strong>. <br/>`
+    document.body.innerHTML += `Qual o último índice da letra A do seu nome? <strong><em>${nome.lastIndexOf('a')}</em></strong>.<br/>`
+    document.body.innerHTML += `As últimas 3 letras do seu nome são: <strong><em>${nome.slice(-3)}</em></strong>.<br/>`
+    document.body.innerHTML += `As palavras do seu nome são: <strong><em>${nome.split(' ')}</em></strong>. <br/>`
+    document.body.innerHTML += `Seu nome com letras maiúsculas: <strong><em>${nome.toUpperCase()}</em></strong>. <br/>`
+    document.body.innerHTML += `Seu nome com letras minúsculas: <strong><em>${nome.toLowerCase()}</em></strong>. <br/>` 
